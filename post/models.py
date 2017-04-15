9f6ca967bd82599dae4cfa6913633255725f5687
Geng <snggeng@gmail.com>
2017-04-14 20:32:31 -0400

    updated models.py with Post class

--- post/models.py
+++ post/models.py
@@ -4,3 +4,8 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+class Post(models.Model):
+    id = models.IntegerField(primary_key=True, unique=True)
+    title = models.CharField(max_length=256, blank=False)
+    link = models.CharField(max_length=512, blank=False)
+    score = models.IntegerField(default=1)
---
+++ post/urls.py
@@ -0,0 +1,4 @@
+from django.conf.urls import urls
+urlpatterns = [
+    url(r'create$')
+]
