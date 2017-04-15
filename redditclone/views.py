bdae6c341b0c86e3eeb6317f26c4c8d6721d6b48
Geng <snggeng@gmail.com>
2017-04-14 20:17:18 -0400

    added css and jquery

--- redditclone/views.py
+++ redditclone/views.py
@@ -3,5 +3,5 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def homepage(request):
-    #render(request, "index.html")
-    return HttpResponse("Hello World")
+    return render(request, "index.html")
+    # return HttpResponse("Hello World")
