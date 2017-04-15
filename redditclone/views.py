11afa2931c823e2236221792634d61e15e5114ed
Geng <snggeng@gmail.com>
2017-04-14 20:59:04 -0400

    update main views.py

--- redditclone/views.py
+++ redditclone/views.py
@@ -1,9 +1,13 @@
 from django.shortcuts import render
 
-from django.http import HttpResponse
+# from django.http import HttpResponse
+from post.models import Post
 
 def homepage(request):
+    allposts = Post.objects.all()
     context = {"name": "Yoyo",
-                "day": "Friday"}
+                "day": "Friday",
+                "posts": allposts}
+
     return render(request, "index.html", context)
     # return HttpResponse("Hello World")
