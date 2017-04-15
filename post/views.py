81c653f8fa79b584abd03db2c2fd732984365708
Geng <snggeng@gmail.com>
2017-04-14 20:43:32 -0400

    added form generator in views.py post

--- post/views.py
+++ post/views.py
@@ -4,3 +4,22 @@ from __future__ import unicode_literals
 from django.shortcuts import render
 
 # Create your views here.
+
+# Post creation utilities
+def generate_form_context(form, is_form, action, old_context={}):
+    old_context['form'] = form
+    old_context['is_form'] = is_form
+    old_context['action'] = action
+    return old_context
+
+
+def render_form(request, template, action, form_type, old_context={}):
+    is_form = True
+    if request.method == 'POST':
+        form = form_type(request.POST)
+        if form.is_valid():
+            is_form = False
+            form.save()
+    else:
+        form = form_type()
+    return render(request, template, generate_form_context(form, is_form, action, old_context=old_context))
