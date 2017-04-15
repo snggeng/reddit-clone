c68f0684b5f4937632474126cf74a681fb0732ba
Geng <snggeng@gmail.com>
2017-04-14 20:35:07 -0400

    ./manage.py makemigrations | migrate

--- post/models.py
+++ post/models.py
@@ -6,6 +6,8 @@ from django.db import models
 # Create your models here.
 class Post(models.Model):
     id = models.IntegerField(primary_key=True, unique=True)
+    
     title = models.CharField(max_length=256, blank=False)
     link = models.CharField(max_length=512, blank=False)
     score = models.IntegerField(default=1)
+    created = models.DateTimeField(auto_now_add=True)
