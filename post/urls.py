4625c397f6edb64eff6214bc6e60e59d7de3ff06
Geng <snggeng@gmail.com>
2017-04-16 21:42:38 -0400

    added urlpatterns += staticfiles_urlpatterns() for deployment

--- post/urls.py
+++ post/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls import url
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 import views
 
@@ -6,3 +7,5 @@ urlpatterns = [
     url(r'create$', views.createpost),
     url(r'upvote/(?P<pid>\d{1,4})$', views.upvote),
 ]
+
+urlpatterns += staticfiles_urlpatterns()
