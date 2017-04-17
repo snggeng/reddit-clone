ea60c6e0ea89a1c74571e6ed3933dc7eaf3864f1
Geng <snggeng@gmail.com>
2017-04-16 21:17:32 -0400

    changed static_root

--- redditclone/settings.py
+++ redditclone/settings.py
@@ -119,6 +119,6 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
-STATIC_ROOT = join(BASE_DIR, 'static')
+STATIC_ROOT = (BASE_DIR,)
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (STATIC_DIR,)
