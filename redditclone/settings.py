37d133faa6cef10ef7b6145617d789de2577b43e
Geng <snggeng@gmail.com>
2017-04-16 21:26:39 -0400

    added static_media directory and changer static_root

--- redditclone/settings.py
+++ redditclone/settings.py
@@ -119,6 +119,6 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
-STATIC_ROOT = (BASE_DIR,)
+STATIC_ROOT = (BASE_DIR, '/static_media')
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (STATIC_DIR,)
